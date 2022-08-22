8dca53c5c32fa60348ff9c5183d27de7c34dbfff
KamilShape <kamilshape@gmail.com>
2022-08-22 17:23:22 +0200

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
-import portrait from './images/portrait.png'
+import portrait from './images/portrait.jpg'
+import landscape from './images/landscape.jpg'
 import './styles/header.css';
 function Header(props) {
   return (
     <div className={props.darkMode ? "header darkMode" : "header"}>
-      <div className="header_container left">
-        <h1 className="header_tittle">Hi! <br /> Welcome to my world!</h1>
-      </div>
-      <div className="header_container right">
-        <h1 className="header_tittle">I invite you to my photo gallery! :)</h1>
-      </div>
-      <div className="header_image-container">
-        <img src={portrait} className="header_image" />
-      </div>
+        <div className="header_container">
+          <div className="header_box first">
+            <h1 className="header_title">Steven</h1>
+            <p className="header_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores distinctio iure amet, inventore reprehenderit omnis velit fuga assumenda exercitationem nobis repellendus modi quam ratione itaque. Tempora minus ullam ex praesentium!</p>
+          </div>
+          <div className="header_box second">
+            <div className='header_image_container'><img className='header_image' src={portrait} alt="" /></div>
+            <div className='header_about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, quidem.</div>
+          </div>
+          <div className="header_box third">
+            <div className='header_about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, quidem.</div>
+            <div className='header_image_container'><img className='header_image' src={landscape} alt="" /></div>
+          </div>
+        </div>
+       
     </div>
   );
 }
